8e5510b47b7fed7f8056ae3f6621d9d9cf48903c
lnunezt <leonkardo.nunez@gmail.com>
2026-07-01 05:12:05 +0000

    Examen Transversal - Actividad 2: Analizador de VLAN

--- examen_transversal_act2.py
+++ examen_transversal_act2.py
@@ -2,8 +2,8 @@ print("--- Analizador de VLANs ---")
 vlan = int(input("Ingrese el número de VLAN a evaluar: "))
 
 if 1 <= vlan <= 1005:
-    print("Es una VLAN de rango Normal")
+    print("Rango Normal")
 elif 1006 <= vlan <= 4094:
-    print("Es una VLAN de rango Extendido")
+    print("Rango Extendido")
 else:
     print("VLAN Inválida")
