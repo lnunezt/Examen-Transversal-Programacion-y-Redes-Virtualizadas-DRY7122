890da67bf68b282d13e4b2e2200384d401aafa8f
lnunezt <leoknardo.nunez@gmail.com>
2026-07-02 10:34:22 -0400

    Add files via upload
    Examen Transversal - Cambio hostname a traves de Netconf

---
+++ ncclient-netconf.py
@@ -0,0 +1,19 @@
+from ncclient import manager
+
+m = manager.connect(
+    host="192.168.56.102",
+    port=830,
+    username="cisco",
+    password="cisco123!",
+    hostkey_verify=False
+    )
+
+netconf_hostname = """
+<config>
+<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+<hostname>PARDO-NUNEZ</hostname>
+</native>
+</config>
+"""
+netconf_reply = m.edit_config (target="running", config=netconf_hostname)
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
